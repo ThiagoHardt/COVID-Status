3904f4aa1cf1d8d50f49e7a6e48bab1b6aea768d
Thiago Hardt <thiago.hardts@gmail.com>
2020-10-08 11:38:40 +0100

    UPDATE
    Dropdown menu to filter countries added.

---
+++ src/assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#searchCountry").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
