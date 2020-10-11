1acd70e69838d5cdfb7556af0d65fabf4012822c
Thiago Hardt <thiago.hardts@gmail.com>
2020-10-11 11:48:32 +0100

    UPDATE
    Sidebar table added

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+  const url = "https://disease.sh/v3/covid-19/countries";
+
   // Creates the dropdown with all countries
-  function createDropdown() {
+  function createDropdownCountries() {
     let dropdown = document.getElementById("dropdownCountries");
     dropdown.length = 0;
 
@@ -9,8 +11,6 @@ $(document).ready(function () {
 
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
-
-    const url = "https://disease.sh/v3/covid-19/countries";
 
     fetch(url)
       .then(function (response) {
@@ -37,5 +37,40 @@ $(document).ready(function () {
       });
   }
 
-  createDropdown();
+  function createTableCountries() {
+    let tbody = document.getElementById("table-body");
+
+    fetch(url)
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.warn(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        response.json().then(function (data) {
+          var countryCases = "";
+
+          // ITERATING THROUGH OBJECTS
+          $.each(data, function (key, value) {
+            //CONSTRUCTION OF ROWS HAVING
+            countryCases += "<tr>";
+            countryCases += "<td>" + value.country + "</td>";
+            countryCases += "<td>" + value.cases + "</td>";
+            countryCases += "<td>" + value.recovered + "</td>";
+            countryCases += "<td>" + value.deaths + "</td>";
+            countryCases += "</tr>";
+          });
+
+          //INSERTING ROWS INTO TABLE
+          $("#table-cases").append(countryCases);
+        });
+      })
+      .catch(function (err) {
+        console.error("Fetch Error -", err);
+      });
+  }
+  createTableCountries();
+  createDropdownCountries();
 });
