1cf7c5118aafd4270d98d63ad892a5dc970b7a4d
Thiago Hardt <thiago.hardts@gmail.com>
2020-10-11 09:02:40 +0100

    UPDATE
    Countries selector added.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,41 @@
 $(document).ready(function () {
-  $("#searchCountry").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $(".dropdown-menu li").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
+  // Creates the dropdown with all countries
+  function createDropdown() {
+    let dropdown = document.getElementById("dropdownCountries");
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement("option");
+    defaultOption.text = "Choose Country";
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    const url = "https://disease.sh/v3/covid-19/countries";
+
+    fetch(url)
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.warn(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        response.json().then(function (data) {
+          let option;
+
+          for (let i = 0; i < data.length; i++) {
+            option = document.createElement("option");
+            option.text = data[i].country;
+            option.value = data[i].countryInfo.iso2;
+            dropdown.add(option);
+          }
+        });
+      })
+      .catch(function (err) {
+        console.error("Fetch Error -", err);
+      });
+  }
+
+  createDropdown();
 });
